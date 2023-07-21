96c5b622c45be721e4a22fd891eab21e701adace
Chris Watton <chriswatton@Chriss-MacBook-Pro.local>
2023-07-21 14:01:11 +0100

    add to reset function and add comments throughout js script

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,8 +1,20 @@
 function lightsOut() {
-    let lightBoxes = document.getElementsByClassName("lights-container");
+    let lightBox = document.getElementsByClassName("lights-container");
     
-    for (let i = 0; i < lightBoxes.length; i++) {
-        lightBoxes[i].getElementsByClassName('light-1')[0].style.backgroundColor = "#dc3545";
+    for (let i = 0; i < lightBox.length; i++) {
+        lightBox[i].getElementsByClassName('light-1')[0].style.backgroundColor = "#dc3545";
     };
-    //start the lights sequence
+    //currently only changes the first light. Needs to cycle through 4 more.
+    //on the fifth light, a duration of 0.2 to 3 seconds is required before the timer starts
+}
+
+function resetGame(){
+    document.getElementById('seconds').innerHTML="00";
+    document.getElementById('tens').innerHTML="00";
+    //Resets times, and lights
+}
+
+function stopGame(){
+    //pauses the timer
+    //a message to congratulate
 }
