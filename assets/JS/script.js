ea67844a15bb58a23e5f1d151355fdae75d916fd
Chris Watton <chriswatton@Chriss-MacBook-Pro.local>
2023-07-21 15:49:52 +0100

    function to turn on lights individually

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,17 +1,20 @@
 function lightsOut() {
-    let lightBox = document.getElementsByClassName("lights-container");
-    
-    for (let i = 0; i < lightBox.length; i++) {
-        lightBox[i].getElementByID("light")[0].style.backgroundColor = "#dc3545";
-    };
-    //currently only changes the first light. Needs to cycle through 4 more.
-    //on the fifth light, a duration of 0.2 to 3 seconds is required before the timer starts
-}
+    const lights = document.getElementsByClassName("light");
+    const delayBetweenLights = 1000; // milliseconds) delay between lights
+  
+    for (let i = 0; i < lights.length; i++) {
+      setTimeout(() => {
+        lights[i].style.backgroundColor = "#dc3545";
+      }, i * delayBetweenLights);
+    }
+  }
+
+  // After all lights are on, switch off and start timer after 0.2 to 3 seconds 
 
 function resetGame(){
     document.getElementById('seconds').innerHTML="00";
     document.getElementById('tens').innerHTML="00";
-    //Resets times, and lights
+    //Resets times
 }
 
 function stopGame(){
