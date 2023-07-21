6c2c24eeed9d039d2ce17f7d4daf845b11c1afb0
Chris Watton <chriswatton@Chriss-MacBook-Pro.local>
2023-07-21 13:32:56 +0100

    complete html page and begin js script

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,4 +1,8 @@
 function lightsOut() {
-    console.log("Hello World!")
+    let lightBoxes = document.getElementsByClassName("lights-container");
+    
+    for (let i = 0; i < lightBoxes.length; i++) {
+        lightBoxes[i].getElementsByClassName('light-1')[0].style.backgroundColor = "#dc3545";
+    };
     //start the lights sequence
 }
